82e65b58264b05ee655907787e0daf422434e06e
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-09-13 16:01:38 +0545

    Changed

--- app/src/main/java/project/entities/Enemy.java
+++ app/src/main/java/project/entities/Enemy.java
@@ -30,6 +30,7 @@ public class Enemy extends Component {
     public Enemy() {
         super();
 
+        //Image
         Image idle_image = new Image("assets/textures/enemy/idle.png");
         Image hurt_image = new Image("assets/textures/enemy/hurt.png");
         Image walk_image = new Image("assets/textures/enemy/walk.png");
@@ -38,53 +39,53 @@ public class Enemy extends Component {
 
         // Death
         animDeathDown = new AnimationChannel(death_image, 8, width, height,
-                Duration.seconds(0.85), 0, 7);
+            Duration.seconds(0.85), 0, 7);
         animDeathUp = new AnimationChannel(death_image, 8, width, height,
-                Duration.seconds(0.85), 8, 15);
+            Duration.seconds(0.85), 8, 15);
         animDeathLeft = new AnimationChannel(death_image, 8, width, height,
-                Duration.seconds(0.85), 16, 23);
+            Duration.seconds(0.85), 16, 23);
         animDeathRight = new AnimationChannel(death_image, 8, width, height,
-                Duration.seconds(0.85), 24, 31);
+            Duration.seconds(0.85), 24, 31);
 
         // Idle
         animIdleUp = new AnimationChannel(idle_image,
-                4, width, height, Duration.seconds(0.75), 4, 7);
+            4, width, height, Duration.seconds(0.75), 4, 7);
         animIdleDown = new AnimationChannel(idle_image,
-                4, width, height, Duration.seconds(0.75), 0, 3);
+            4, width, height, Duration.seconds(0.75), 0, 3);
         animIdleLeft = new AnimationChannel(idle_image,
-                4, width, height, Duration.seconds(0.75), 8, 11);
+            4, width, height, Duration.seconds(0.75), 8, 11);
         animIdleRight = new AnimationChannel(idle_image,
-                4, width, height, Duration.seconds(0.75), 12, 15);
+            4, width, height, Duration.seconds(0.75), 12, 15);
 
         // Hurt
         animHurtDown = new AnimationChannel(hurt_image, 6, width, height,
-                Duration.seconds(0.64), 0, 5);
+            Duration.seconds(0.64), 0, 5);
         animHurtUp = new AnimationChannel(hurt_image, 6, width, height,
-                Duration.seconds(0.64), 6, 11);
+            Duration.seconds(0.64), 6, 11);
         animHurtLeft = new AnimationChannel(hurt_image, 6, width, height,
-                Duration.seconds(0.64), 12, 17);
+            Duration.seconds(0.64), 12, 17);
         animHurtRight = new AnimationChannel(hurt_image, 6, width, height,
-                Duration.seconds(0.64), 18, 23);
+            Duration.seconds(0.64), 18, 23);
 
         // Walk
         animWalkUp = new AnimationChannel(walk_image, 6, width, height,
-                Duration.seconds(0.64), 6, 11);
+            Duration.seconds(0.64), 6, 11);
         animWalkDown = new AnimationChannel(walk_image, 6, width, height,
-                Duration.seconds(0.64), 0, 5);
+            Duration.seconds(0.64), 0, 5);
         animWalkLeft = new AnimationChannel(walk_image, 6, width, height,
-                Duration.seconds(0.64), 12, 17);
+            Duration.seconds(0.64), 12, 17);
         animWalkRight = new AnimationChannel(walk_image, 6, width, height,
-                Duration.seconds(0.64), 18, 23);
+            Duration.seconds(0.64), 18, 23);
 
         // Attack
         animAttackDown = new AnimationChannel(attack_image, 8, width, height,
-                Duration.seconds(0.64), 0, 7);
+            Duration.seconds(0.64), 0, 7);
         animAttackUp = new AnimationChannel(attack_image, 8, width, height,
-                Duration.seconds(0.64), 8, 15);
+            Duration.seconds(0.64), 8, 15);
         animAttackLeft = new AnimationChannel(attack_image, 8, width, height,
-                Duration.seconds(0.64), 16, 23);
+            Duration.seconds(0.64), 16, 23);
         animAttackRight = new AnimationChannel(attack_image, 8, width, height,
-                Duration.seconds(0.64), 24, 31);
+            Duration.seconds(0.64), 24, 31);
 
         texture = new AnimatedTexture(animIdleUp);
         texture.loop();
